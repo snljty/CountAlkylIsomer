39d92c0db9ef92cc9154b768a222fbbe4b1d1a0e
snljty <snljty@gmail.com>
2022-03-03 16:47:23 +0800

    tiny fix of output format

--- CountAlkylAndAlkaneIsomersWithoutStereochemistry.f90
+++ CountAlkylAndAlkaneIsomersWithoutStereochemistry.f90
@@ -27,20 +27,20 @@ module global
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   implicit none
-  integer, PARAMETER :: MAX_CARBON = 30
-  integer, PARAMETER :: CALC_CARBON = 20
-  integer :: A(0: MAX_CARBON)
-  integer :: P(0: MAX_CARBON), Q(0: MAX_CARBON)
-  integer :: C(0: MAX_CARBON)
-end module
+  integer(kind=4), parameter :: MAX_CARBON = 30
+  integer(kind=4), parameter :: CALC_CARBON = 20
+  integer(kind=4) :: A(0: MAX_CARBON)
+  integer(kind=4) :: P(0: MAX_CARBON), Q(0: MAX_CARBON)
+  integer(kind=4) :: C(0: MAX_CARBON)
+end module global
 
 program main
   use global
   implicit none
 
-  integer :: NUM = 0
+  integer(kind=4) :: NUM = 0
 
-  do NUM = 0, MAX_CARBON, 1
+  do NUM = 0, MAX_CARBON
     A(NUM) = 0
     P(NUM) = 0
     Q(NUM) = 0
@@ -48,62 +48,64 @@ program main
   end do
 
   call Calc_A_m(0)
-  do NUM = 1, CALC_CARBON, 1
+  do NUM = 1, CALC_CARBON
     call Calc_A_m(NUM)
     call Calc_P_m(NUM)
     call Calc_Q_m(NUM)
     call Calc_C_m(NUM)
   end do
-  write(*, "(A, A, A, I2, A, I8)") "C", "  ", "H", (2 * 1 + 1), " -: ", A(1)
-  do NUM = 2, CALC_CARBON, 1
-    write(*, "(A, I2, A, I2, A, I8)") "C", NUM, "H", (2 * NUM + 1), " -: ", A(NUM)
+  NUM = 1
+  write(*, "(A,A,I0,A,1X,I0)") "C", "H", 2 * NUM + 1, "-:", A(NUM)
+  do NUM = 2, CALC_CARBON
+    write(*, "(A,I0,A,I0,A,1X,I0)") "C", NUM, "H", 2 * NUM + 1, "-:", A(NUM)
   end do
-  write(*, *) ""
-  write(*, "(A, A, A, I2, A, I8)") "C", "  ", "H", (2 * 1 + 2), " : ", C(1)
-  do NUM = 2, CALC_CARBON, 1
-    write(*, "(A, I2, A, I2, A, I8)") "C", NUM, "H", (2 * NUM + 2), " : ", C(NUM)
+  write(*, "()")
+  NUM = 1
+  write(*, "(A,A,I0,A,1X,I0)") "C", "H", 2 * NUM + 2, ":", C(NUM)
+  do NUM = 2, CALC_CARBON
+    write(*, "(A,I0,A,I0,A,1X,I0)") "C", NUM, "H", 2 * NUM + 2, ":", C(NUM)
   end do
 
   stop
-end program
+end program main
 
 subroutine TryCalc_A_m(M)
   use global
   implicit none
 
-  integer :: M
+  integer(kind=4) :: M
   ! M >= 0 for A(M)
-  integer :: I = 0
+  integer(kind=4) :: I = 0
 
   do I = 0, m, 1
-    if(A(I) == 0) then
+    if (A(I) == 0) then
       call Calc_A_m(i)
     end if
   end do
   return
-end subroutine
+end subroutine TryCalc_A_m
 
 subroutine Calc_A_m(M)
   use global
   implicit none
 
-  integer :: M
+  integer(kind=4) :: M
   !  M >= 0 for A(M)
-  integer :: T = 0, ANS = 0
-  integer :: I = 0, J = 0
+  integer(kind=4) :: T = 0, ANS = 0
+  integer(kind=4) :: I = 0, J = 0
 
   !  A(0) = 1
   ANS = 0
-  if(M == 0) then
+  if (M == 0) then
     A(M) = 1
     return
   end if
   !  term 1: x * A(x)^3
   T = 0
   I = 0
-  do while(I < M)
+  do while (I < M)
     J = 0
-    do while(I + J < M)
+    do while (I + J < M)
       T = T + A(I) * A(J) * A(M - 1 - I - J)
       J = J + 1
     end do
@@ -113,14 +115,14 @@ subroutine Calc_A_m(M)
   !  term 2: x * 3 * A(x) * A(x^2)
   T = 0
   J = 0
-  do while(2 * J < M)
+  do while (2 * J < M)
     T = T + A(M - 1 - 2 * J) * A(J)
     J = J + 1
   end do
   ANS = ANS + 3 * T
   ! term 3: x * 2 * A(x^3)
   T = 0
-  if(mod((M - 1), 3) == 0) then
+  if (mod((M - 1), 3) == 0) then
     T = A((M - 1) / 3)
   end if
   ANS = ANS + 2 * T
@@ -128,46 +130,45 @@ subroutine Calc_A_m(M)
   ANS = ANS / 6
   A(M) = ANS
   return
-end subroutine
+end subroutine Calc_A_m
 
 subroutine TryCalc_P_m(M)
   use global
   implicit none
 
-  integer :: M
+  integer(kind=4) :: M
   !  M >= 1 for P(M)
-  integer :: I = 0
+  integer(kind=4) :: I = 0
 
   do I = 0, M, 1
-    if(A(I) == 0) then
+    if (A(I) == 0) then
       call Calc_A_m(I)
     end if
   end do
-  if(P(M) == 0) then
+  if (P(M) == 0) then
     call Calc_P_m(M)
   end if
   return
-end subroutine
+end subroutine TryCalc_P_m
 
 subroutine Calc_P_m(M)
   use global
   implicit none
 
-  integer :: M
-
+  integer(kind=4) :: M
   !  M >= 1 for P(M)
-  integer :: T = 0, ANS = 0
-  integer :: I = 0, J = 0, K = 0
+  integer(kind=4) :: T = 0, ANS = 0
+  integer(kind=4) :: I = 0, J = 0, K = 0
 
   ANS = 0
   !  term 1: x * A(x)^4
   T = 0
   I = 0
-  do while(I < M)
+  do while (I < M)
     J = 0
-    do while(I + J < M)
+    do while (I + J < M)
       K = 0
-      do while(I + J + K < M)
+      do while (I + J + K < M)
         T = T + A(I) * A(J) * A(K) * A(M - 1 - I - J - K)
         K = K + 1
       end do
@@ -179,9 +180,9 @@ subroutine Calc_P_m(M)
   !  term 2: x * 6 * A(x)^2 * A(x^2)
   T = 0
   K = 0
-  do while(2 * K < M)
+  do while (2 * K < M)
     I = 0
-    do while(I + 2 * K < M)
+    do while (I + 2 * K < M)
       T = T + A(I) * A(M - 1 - I - 2 * K) * A(K)
       I = I + 1
     end do
@@ -190,9 +191,9 @@ subroutine Calc_P_m(M)
   ANS = ANS + 6 * T
   !  term 3: x * 3 * A(x^2)^2
   T = 0
-  if(mod((M - 1), 2) == 0) then
+  if (mod((M - 1), 2) == 0) then
     I = 0
-    do while(2 * I < M)
+    do while (2 * I < M)
       T = T + A(I) * A((M - 1) / 2 - I)
       I = I + 1
     end do
@@ -201,14 +202,14 @@ subroutine Calc_P_m(M)
   !  term 4: x * 8 * A(x) * A(x^3)
   T = 0
   J = 0
-  do while(3 * J < M)
+  do while (3 * J < M)
     T = T + A(M - 1 - 3 * J) * A(J)
     J = J + 1
   end do
   ANS = ANS + 8 * T
   !  term 5: x * 6 * A(x^4)
   T = 0
-  if(mod((M - 1), 4) == 0) then
+  if (mod((M - 1), 4) == 0) then
     T = A((M - 1) / 4)
   end if
   ANS = ANS + 6 * T
@@ -216,39 +217,37 @@ subroutine Calc_P_m(M)
   ANS = ANS / 24
   P(M) = ANS
   return
-end subroutine
+end subroutine Calc_P_m
 
 subroutine TryCalc_Q_m(M)
   use global
   implicit none
 
-  integer :: M
-
+  integer(kind=4) :: M
   !  M >= 1 for Q(M)
-  integer :: I = 0
+  integer(kind=4) :: I = 0
 
   do I = 0, M, 1
-    if(A(I) == 0) then
+    if (A(I) == 0) then
       call Calc_A_m(I)
     end if
   end do
-  if(Q(M) == 0 .AND. M /= 1) then
+  if (Q(M) == 0 .AND. M /= 1) then
     call Calc_Q_m(M)
   end if
   return
-end subroutine
+end subroutine TryCalc_Q_m
 
 subroutine Calc_Q_m(M)
   use global
   implicit none
 
-  integer :: M
-
+  integer(kind=4) :: M
   !  M >= 1 for Q(M)
-  integer :: T = 0, ANS = 0
-  integer :: I = 0
+  integer(kind=4) :: T = 0, ANS = 0
+  integer(kind=4) :: I = 0
 
-  if(M == 1) then
+  if (M == 1) then
     Q(M) = 0
     return
   end if
@@ -261,7 +260,7 @@ subroutine Calc_Q_m(M)
   ANS = ANS + T
   !  term 2: A(x^2)
   T = 0
-  if(mod(M, 2) == 0) then
+  if (mod(M, 2) == 0) then
     T = A(M / 2)
   end if
   ANS = ANS + T
@@ -271,55 +270,54 @@ subroutine Calc_Q_m(M)
   ANS = ANS - A(M)
   Q(M) = ANS
   return
-end subroutine
+end subroutine Calc_Q_m
 
 subroutine TryCalc_C_m(M)
   use global
   implicit none
 
-  integer :: M
-
+  integer(kind=4) :: M
   !  M >= 1 for C(M)
-  integer :: I = 1
+  integer(kind=4) :: I = 1
 
-  if(A(0) == 0) then
+  if (A(0) == 0) then
     call Calc_A_m(0)
   end if
-  if(A(1) == 0) then
+  if (A(1) == 0) then
     call Calc_A_m(1)
   end if
-  if(P(1) == 0) then
+  if (P(1) == 0) then
     call Calc_P_m(1)
   end if
   do I = 2, M, 1
-    if(A(I) == 0) then
+    if (A(I) == 0) then
       call Calc_A_m(I)
     end if
-    if(P(I) == 0) then
+    if (P(I) == 0) then
       call Calc_P_m(I)
     end if
-    if(Q(I) == 0) then
+    if (Q(I) == 0) then
       call Calc_Q_m(I)
     end if
   end do
   return
-end subroutine
+end subroutine TryCalc_C_m
 
 subroutine Calc_C_m(M)
   use global
   implicit none
 
-  integer :: M
-
+  integer(kind=4) :: M
   !  M >= 1 for C(M)
-  integer :: ANS = 0
+  integer(kind=4) :: ANS = 0
+
   ANS = 0
-  if(mod(M, 2) == 0) then
+  if (mod(M, 2) == 0) then
     ANS = P(M) - Q(M) + A(M / 2)
   ELSE
     ANS = P(M) - Q(M)
   end if
   C(M) = ANS
   return
-end subroutine
+end subroutine Calc_C_m
 
